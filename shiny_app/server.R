623dc36958482f587e1302a4a959cc7c9c18c61c
mikeacaballero <mcaballerophd@gmail.com>
2018-05-01 17:36:11 -0400

    upload app code

---
+++ shiny_app/server.R
@@ -0,0 +1,64 @@
+#### Setup ####
+library(shiny)
+library(DT)
+#### server.R ####
+
+function(input, output, session) {
+
+  # absolute vs relative capacity of electric power plants, 1999-2014
+  output$capacity_plot <- renderPlot({
+    delta_capacity %>%  
+      filter() %>%
+      ggplot(aes(x = abs_capacity, y = rel_capacity)) +   
+      geom_point(aes(col = region), size = 2) +
+      scale_x_log10(breaks = NULL) + scale_y_log10(breaks = NULL) +
+      theme_bw() +
+      labs(x = "----- Increasing capacity (kW, thousand) ----->",
+           y = "----- Increaasing capacity (%) ----->",
+           title = "Changes in electric capacity by country, 1999-2014") +
+      theme(text = element_text(size = 24))
+      
+  })  
+  
+  #interactive feature: selecting countries
+    output$brush_info <- renderPrint({
+    brushedPoints(delta_capacity, input$plot1_brush)
+  })
+  
+  # dissecting country electricity capacity
+  output$country_plot <- renderPlot({
+    electricity %>%
+      filter(country_or_area == input$country_select,
+             capacity != "total_capacity") %>%
+      ggplot(aes(x = year, y = value, color = capacity)) +
+      geom_line(size = 2) +
+      theme_bw() +
+      labs(x = "Year",
+           y = "Electric capacity (kW, thousands)",
+           title = "Changes in electric capacity by country, 1999-2014") +
+      theme(text = element_text(size = 24))
+                          
+  })
+
+### annualized histogram of non-combustible electricity capacity index
+  output$renewables_plot <- renderPlot({
+    electricity %>% 
+      filter(year == input$region_commodity_year, 
+             capacity == "total_capacity") %>% 
+      select(alpha_3, region, non_combust_index) %>%   
+      group_by(region) %>% 
+      ggplot(aes(x = non_combust_index)) +
+      geom_histogram(aes(fill = region), binwidth = 0.1) +
+      ylim(0,20) +
+      facet_wrap(~ region, ncol = 3) +
+      theme_bw() +
+      theme(text = element_text(size = 24)) +
+      labs(x = "Non-Fossil Fuel Index")
+  })
+  
+  # data table for reference
+  output$table <- DT::renderDataTable({
+    datatable(electricity, rownames=FALSE) %>% 
+      formatStyle(input$selected, fontWeight='bold')
+  })
+}
---
+++ shiny_app/ui.R
@@ -0,0 +1,132 @@
+#### Setup ####
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(plotly)
+#### ui.R ####
+
+dashboardPage(
+  skin = "yellow",
+  dashboardHeader(title = "UN Electricity Explorer"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Explore", tabName = "explore", icon = icon("globe")),
+      menuItem("Renewables", tabName = "renewables", icon = icon("recycle")),
+      menuItem("Analyze", tabName = "analyze", icon = icon("bolt")),
+      menuItem("Data", tabName = "data", icon = icon("table"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "explore",
+        fluidRow(
+          column(width = 3, 
+            box(title = "Which countries have invested in electricity production?",
+                width = NULL, 
+                solidHeader = TRUE,
+                status = "warning",
+                "This graph visualizes changes in electricity infrastructure for
+                countries (points) and regions (colors) from 1999 - 2014. 
+                "),
+            box(title = "Rationale: Energy Infrastructure",
+                width = NULL,
+                status = "warning",
+                "Building a power plant represents a long-term committment to that energy strategy.",
+                br(),
+                "Identifying growth countries for trading resources or promoting alternative
+                energy strategies could help achieve economic and environmental goals.",
+                hr(),
+                "This UN energy statistics dataset is bookended by the Kyoto treaty and Paris agreement.",
+                tags$a(href="https://www.cnn.com/2013/07/26/world/kyoto-protocol-fast-facts/index.html", "See timeline.")
+                )
+          ),
+          column(width = 9,
+            box(title = NULL,
+                width = NULL,
+                background = "yellow",
+                height = 600,
+            plotOutput(outputId = "capacity_plot",
+                     height = 580,
+                     brush = brushOpts(
+                       id = "plot1_brush",
+                       resetOnNew = TRUE))),
+            
+            box(title = "Select countries above",
+                width = NULL,
+                status = "warning",
+                verbatimTextOutput("brush_info")) # stretch printout?
+          )
+        )
+      ), 
+      tabItem(tabName = "analyze",
+        fluidRow(
+          column(width = 3,
+                 box(title = "Dissecting Electric Capacity by Country",
+                     width = NULL, 
+                     solidHeader = TRUE, 
+                     status = "warning",
+                     "2-sentence introduction to the question."
+                 ),
+                  box(title = "Select inputs",
+                     width = NULL, 
+                     status = "warning",
+                     selectizeInput('country_select', 'Select a country:', 
+                                    choices = electricity$country_or_area,
+                                    selected = "France"))
+          ),
+          column(width = 9,
+                 box(title = NULL,
+                     width = NULL,
+                     height = 600,
+                     background = "yellow",
+                     plotOutput(outputId = "country_plot",
+                                height = 580,
+                                width = NULL)))
+        )
+      ),
+      tabItem(tabName = "renewables",
+        fluidRow(
+          column(width = 3,
+                  box(title = "How has non-combustible electricity capacity changed?",
+                      width = NULL, 
+                      solidHeader = TRUE, 
+                      status = "warning",
+                      "The non-combustible index is proportion of electric capacity that
+                      does not explicity rely on fossil fuels normalized by total capacity.",
+                      br(),
+                      "Changing electricity infrastructure is slow and expensive, but some
+                      regions are changing energy strategies faster than others.",
+                      hr(),
+                      "Click the play button below, or select a year of interest."),
+                  box(title = "Select inputs",
+                      width = NULL, 
+                      status = "warning",
+                      sliderInput("region_commodity_year", "Year:",
+                                  min = 1999, max = 2014,
+                                  value = 1999, step = 1,
+                                  sep = "", animate = TRUE))
+                ),
+                column(width = 9,
+                       box(width = NULL,
+                           height = 600,
+                           background = "yellow",
+                           plotOutput(outputId = "renewables_plot", height = "580px")
+                      )
+                )
+        )
+    ),
+    tabItem(tabName = "data",
+            fluidRow(
+              column(width = 12,
+                     box(title = "Data Table for reference",
+                         width = NULL, 
+                         solidHeader = TRUE, 
+                         status = "warning",
+                         DT::dataTableOutput("table"),
+                          tags$a(href="https://github.com/mikeacaballero", "Check out my GitHub for code & data!"))
+            )
+    )
+)
+)
+)
+)
